30b0bfb2e2579e80ce1f0cbbf6a03743425e1d03
marcelosvloja20-cyber <marcelosvloja20@gmail.com>
2026-02-22 22:12:44 -0300

    Create chains.js

---
+++ chains.js
@@ -0,0 +1,41 @@
+const CHAINS = {
+
+polygon: {
+
+name: "Polygon",
+
+chainId: "0x89",
+
+rpc: "https://polygon-rpc.com",
+
+symbol: "POL"
+
+},
+
+ethereum: {
+
+name: "Ethereum",
+
+chainId: "0x1",
+
+rpc: "https://rpc.ankr.com/eth",
+
+symbol: "ETH"
+
+},
+
+bsc: {
+
+name: "BNB Chain",
+
+chainId: "0x38",
+
+rpc: "https://rpc.ankr.com/bsc",
+
+symbol: "BNB"
+
+}
+
+};
+
+export default CHAINS;
